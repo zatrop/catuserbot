ac61eba2df257bc6b8a77b1f90245759fe1d5979
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2022-02-13 19:23:41 +0530

    Update utils.py

--- userbot/helpers/utils/utils.py
+++ userbot/helpers/utils/utils.py
@@ -3,7 +3,7 @@ import functools
 import shlex
 from typing import Tuple
 
-from telethon import functions, types
+from telethon import _tl
 
 from ...core.logger import logging
 
@@ -43,8 +43,8 @@ def runasync(func: callable):
 async def unsavegif(event, sandy):
     try:
         await event.client(
-            functions.messages.SaveGifRequest(
-                id=types.InputDocument(
+            _tl.fn.messages.SaveGifRequest(
+                id=_tl.InputDocument(
                     id=sandy.media.document.id,
                     access_hash=sandy.media.document.access_hash,
                     file_reference=sandy.media.document.file_reference,
